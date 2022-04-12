5fff9835c52d25696638c9b2618cedbc57798f43
Ubuntu <maddie.madlad@gmail.com>
2022-04-12 15:04:06 +0200

    navbar is new module, php test

--- resources/templates/app.php
+++ resources/templates/app.php
@@ -1,4 +1,4 @@
-<DOCTYPE html>
+<!DOCTYPE html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -8,23 +8,9 @@
     </head>
     <body>
 
-        <nav class="navbar">
-            <div class="logo">
-                <h1>Crash</h1>
-                <h2>クラッシュ</h2>
-            </div>
-            <div class="container">
-                <div class="search-box">
-                    <p>maddieuwu@search<mark class="info">:</mark>~<mark class="info">$</mark><input type="text"></p>
-                </div>
-                <ul>
-                    <li><a class="is-active" href="">Home</a></li>
-                    <li><a href="">Explore trends</a></li>
-                    <li><a href="">Help</a></li>
-                    <li><a href="">About the project</a></li>
-                </ul>
-            </div>
-        </nav>
+    <?php
+        echo "My first PHP script!";
+    ?>
         <div class="columns">
             <div class="column">
             </div>
