56f4c182e3e9e96f4bb8139086252e6216ef56f4
Ubuntu <maddie.madlad@gmail.com>
2022-04-15 09:45:51 +0200

    more reorganizing

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 
     <?php
         require ".env";
-        include $templates['default'];
+        require "resources/elements/app.php"
     ?>
+<!DOCTYPE html>
+    <?php
+        include $modules['head']
+    ?>
+    <body>
+        <?php
+            include $modules['navbar']
+        ?>
+        <div class="columns">
+            <div class="column">
+
+            </div>
+            <div class="column is-1-4">
+                
+            </div>
+        </div>
+    </body>
---
+++ resources/elements/app.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file holds global variables, that are
+ * meant to be a convinient shortcuts
+ */
+
+$locations=array(
+        "elements"=>"resources/elements/"
+    );
+$modules=array(
+        "navbar"=>$locations['elements']."modules/navbar.php",
+        "head"=>$locations['elements']."modules/head.php"
+    );
+
+?>
