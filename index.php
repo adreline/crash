9a575a834b15b80377fe533f6e7fc5a6015046eb
Ubuntu <maddie.madlad@gmail.com>
2022-04-17 17:44:05 +0200

    first db integration test

--- index.php
+++ index.php
@@ -1,19 +1,43 @@
 
     <?php
         require "resources/elements/app.php";
-        include $modules['database'];
+        include Helper::$modules['fandom'];
+        include Helper::$modules['publication'];
     ?>
 <!DOCTYPE html>
     <?php
-        include $modules['head'];
+        include Helper::$modules['head'];
     ?>
     <body>
         <?php
-            include $modules['navbar'];
+            include Helper::$modules['navbar'];
         ?>
         <div class="columns">
             <div class="column">
-               <?php  getTest(); ?>
+                <div class="window">
+                <h2>Popular fandoms</h2>
+                <div class="content">
+                   <?php
+                        foreach(Fandom::getFandom() as $fandom){
+                            echo "<p>>$fandom->friendly_name (<mark class=\"info\">2312</mark>)</p>";
+                        }
+                    ?>
+ 
+                </div>
+                </div>
+                <div class="window">
+                <h2>Recent works</h2>
+                <div class="content">
+                   <?php
+                        foreach(Publication::getPublication(null,"LIMIT 4") as $publication){
+                            $title=$publication->title;
+                            $paragraphs=array("test","of parandjasd ansd ojansdioasn oasndo iasodja");
+                            include Helper::$modules['post'];
+                        }
+                    ?>
+ 
+                </div>
+                </div>
             </div>
             <div class="column is-1-4">
                 
