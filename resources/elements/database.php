00a28aac56f5d3f30153705062919b506d8a4789
Ubuntu <maddie.madlad@gmail.com>
2022-04-17 13:50:22 +0200

    defined insert sql methods

--- resources/elements/database.php
+++ resources/elements/database.php
@@ -1,9 +1,15 @@
 <?php
     require "crash/.env";
+
     define("METHODS",array(
         "test" => "SELECT * FROM test"
     ));
-    
+    define("INSERT_METHODS",array(
+      'users'=>"INSERT INTO `users` (`id_user`, `username`, `password`, `time_stamp`) VALUES (NULL, '%0', '%1', CURRENT_TIMESTAMP)",
+      'leaflet'=>"INSERT INTO `leafs` (`id_leaf`, `body`, `publications_id_publication`) VALUES (NULL, '%0', '%1')",
+      'fandom'=>"INSERT INTO `fandoms` (`id_fandom`, `friendly_name`, `name`) VALUES (NULL, '%0', '%1')",
+      'publication'=>"INSERT INTO `publications` (`id_publication`, `title`, `planned_length`, `status`, `time_stamp`, `users_id_user`, `fandoms_id_fandom`) VALUES (NULL, '%0', '%1', '%2', CURRENT_TIMESTAMP, '%3', '%4')"
+    ));
 
     function connect(){
         $c =  mysqli_connect(DB_CONF['host'],DB_CONF['database_user'],DB_CONF['database_pass'],DB_CONF['database']);
@@ -25,7 +31,7 @@
         } else {
           echo "0 results";
         }
-        
+
         mysqli_close($c);
     }
     */
