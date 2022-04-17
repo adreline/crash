aee32c48c20240ac92af9c5e32b7aef66fbc894b
Ubuntu <maddie.madlad@gmail.com>
2022-04-17 14:57:38 +0200

    moved helper function fill_in to app.php

--- resources/database/factory.php
+++ resources/database/factory.php
@@ -1,5 +1,6 @@
 <?php
     include "../elements/database.php";
+    include "../elements/app.php";
     define("USERNAME_POOL",explode(" ","rope beauty mourn tired throughout roll tap paint sentence mom defeat roll salt nice beard act boyfriend worry stun angel describe glass desperately point army within advice morning goose kid football answer skip half feather quite cookie trust vain peace lazy easy puzzle easy lazy blind vain lot thrown rise rough near prince afraid honey trust murder return begun rose mourn held feather held subject drive cease blind bump sad screw quite advice half claw hour group fact machine window stair blind fog music yesterday paint neither emotion wow beneath bump return wow afraid neighbor hour canvas shine limb question faint within shore drop doll chance advice quite shimmer quite insult children remove flow serious dad peaceful kid cookie street cease secret illuminate apart steady notice poetry point shore silence cheer bright rich drop rough drop arrive follow nearly path price empty peaceful whisper guilt notice screw anymore tough song tease earth goal both glare both limb beauty tangle shine illuminate began dove began thrown notice hum already led brother insult sorry cease house appreciate brother mourn apart mass heard student shine carve began shield began insult shine sanctuary heard stolen already shiny okay nearly color feather earth hatred earth commit song crystal color delight color yesterday okay hop dear throughout okay army anymore army almost subject along moonlight push bank step guard second clutch reach cheer reach cheer reason clutch fire clutch line commit guy crystal today delight land doll finally doll young"));
     define("UPOOL_ENTROPY", count(USERNAME_POOL));
     define("ALPHA",explode(" ","q w e r t y u i o p a s d f g h j k l z x c v b n m"));
@@ -9,18 +10,7 @@
     function str_contains(string $haystack, string $needle): bool{
       return '' === $needle || false !== strpos($haystack, $needle);
     }
-    function fill_in($str,$data){
-      //this funct inserts data into strings at specified places
-      $head="%0";
-      $pos=0;
-      while (str_contains($str,$head)) {
 
-        $str=str_replace($head,$data[$pos],$str);
-        $pos++;
-        $head="%$pos";
-      }
-      return $str;
-    }
     function genRandomWord(){
       //this helper funct gens random words
       //a word is on average 2~7 char. long
--- resources/elements/app.php
+++ resources/elements/app.php
@@ -12,5 +12,17 @@ $modules=array(
         "head"=>$locations['elements']."modules/head.php",
         "database"=>$locations['elements']."database.php"
     );
+//helper functions 
+    function fill_in($str,$data){
+      //this funct inserts data into strings at specified places
+      $head="%0";
+      $pos=0;
+      while (str_contains($str,$head)) {
+        $str=str_replace($head,$data[$pos],$str);
+        $pos++;
+        $head="%$pos";
+      }
+      return $str;
+    }
 
 ?>
