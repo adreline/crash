e12d2697007e5efdd32d0104276befd8ae3b3f87
Ubuntu <maddie.madlad@gmail.com>
2022-04-17 15:48:23 +0200

    Database is now static class

--- resources/elements/database.php
+++ resources/elements/database.php
@@ -1,38 +1,46 @@
 <?php
     require "crash/.env";
-
-    define("METHODS",array(
-        "test" => "SELECT * FROM test"
-    ));
+    
     define("INSERT_METHODS",array(
       'users'=>"INSERT INTO `users` (`id_user`, `username`, `password`, `time_stamp`) VALUES (NULL, '%0', '%1', CURRENT_TIMESTAMP)",
       'leaflet'=>"INSERT INTO `leafs` (`id_leaf`, `body`, `publications_id_publication`) VALUES (NULL, '%0', '%1')",
-      'fandom'=>"INSERT INTO `fandoms` (`id_fandom`, `friendly_name`, `name`) VALUES (NULL, '%0', '%1')",
       'publication'=>"INSERT INTO `publications` (`id_publication`, `title`, `planned_length`, `status`, `time_stamp`, `users_id_user`, `fandoms_id_fandom`) VALUES (NULL, '%0', '%1', '%2', CURRENT_TIMESTAMP, '%3', '%4')"
     ));
-
-    function connect(){
-        $c =  mysqli_connect(DB_CONF['host'],DB_CONF['database_user'],DB_CONF['database_pass'],DB_CONF['database']);
-        if(!$c){
-            die("Connection error");
-        }else{
-            return $c;
+    class Database{
+        private static function connect(){
+            $c =  mysqli_connect(DB_CONF['host'],DB_CONF['database_user'],DB_CONF['database_pass'],DB_CONF['database']);
+            if(!$c){
+                die("Connection error");
+            }else{
+                return $c;
+            }
         }
-    }
-    /*
-    function getTest(){
-        $c = connect();
-        $result = mysqli_query($c, METHODS['test']);
-        if (mysqli_num_rows($result) > 0) {
-          // output data of each row
-          while($row = mysqli_fetch_assoc($result)) {
-            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-          }
-        } else {
-          echo "0 results";
+        public static function select($method,$f){
+            $c = Database::connect();
+            $results;
+            $result = mysqli_query($c, $method);
+            if (mysqli_num_rows($result) > 0) {
+                // output data of each row
+                while($row = mysqli_fetch_assoc($result)) { 
+                    $results[]=$f($row);
+                }
+            } else {
+                echo "0 results";
+            }
+            mysqli_close($c);
+            return $results;
         }
-
-        mysqli_close($c);
+        public static function insert($sql){
+            $c = Database::connect();
+            if ($c->query($sql) === TRUE) {
+                $c->close();
+                return true;
+            } else {
+                $c->close();
+                return false;
+            }
+        }
+        
     }
-    */
+    //main functions
 ?>
