b54b8f14b4faa647c813aa6c13afc1bb3b97006b
Ubuntu <maddie.madlad@gmail.com>
2022-04-17 19:12:06 +0200

    hotfix

--- resources/elements/modules/post.php
+++ resources/elements/modules/post.php
@@ -2,11 +2,7 @@
                 <img src="img/91026b4802025c0ac38329e7cab1c113.jpg" alt="">
                 <div class="content">
                   <h3><?php echo $title; ?></h3>
-                    <?php 
-                      foreach($paragraphs as $body){
-                        echo "<p>$body</p>";
-                      }
-                    ?>
+                    <p><?php echo $prompt; ?></p>
                   <div class="container">
                     <p>Replies: <mark class="info">7</mark> </p>
                     <p>Kudos: <mark class="info">77</mark> </p>
