81fc93438c3b01a5631c9cd9383362e6242c5e2c
Ubuntu <maddie.madlad@gmail.com>
2022-04-19 19:24:54 +0200

    css tweaks and added content to about page

--- resources/views/about.php
+++ resources/views/about.php
@@ -6,14 +6,29 @@
         <?php
             include Crash::$module['navbar'];
         ?>
-        <div class="columns">
-            <div class="column">
-                <div class="window">
-                <h2>About this page</h2>
-                <div class="content">
- 					
+                
+
+                <div class="columns">
+                	<div class="column">
+                		<div class="window">
+                			<h2><mark class="success">></mark>Full documentation</h2>
+                			<div class="content">
+                				<ul>
+                					<li data-icon="#"> <a href="https://maddie-nie.atlassian.net/l/c/8jAUCsUe"> <mark class="success">Confluence project wiki</mark> </a> </li>
+                					<li data-icon="#"> <a href="https://bitbucket.org/adreline/crash/src/master/"> <mark class="success">Code repository</mark> </a> </li>
+                				</ul>
+                			</div>
+                		</div>
+                	</div>
+                	<div class="column">
+                		<div class="window">
+			                <h2><mark class="success">$</mark>About the project</h2>
+				                <div class="content">
+				                	<p>Developed for the community of transformative writters. Crash is a small, fast, and secure CMS written entirely in <mark class="info">PHP</mark>.</p>
+				                	<p>It's meant to be used for publishing written works of fan fiction, but it's fully capable of being expanded upon to fit any micro-blogging requirements. </p>
+				                	<p>It's small, fast, and flexible – what more could you want?</p>
+				                </div>
+			             </div>
+                	</div>
                 </div>
-                </div>
-            </div>
-        </div>
     </body>
