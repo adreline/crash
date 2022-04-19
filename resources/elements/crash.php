eba008af09fedc3006d116d84280b21038da995b
Ubuntu <maddie.madlad@gmail.com>
2022-04-19 18:26:45 +0200

    created static about page

--- resources/elements/crash.php
+++ resources/elements/crash.php
@@ -18,7 +18,8 @@ class Crash{
     );
     //This constant, defines static pages that the app recognizes. useful if you need to add a page that can't be created dynamically.
     public static $static_page=array(
-        "home" => "resources/views/home.php"
+        "home" => "resources/views/home.php",
+        "about" => "resources/views/about.php"
     );
     //Primitive templating files with fixed variables to fill. you can add new ones and use them in controllers
     public static $template=array(
---
+++ resources/views/about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <?php
+        include Crash::$module['head'];
+    ?>
+    <body>
+        <?php
+            include Crash::$module['navbar'];
+        ?>
+        <div class="columns">
+            <div class="column">
+                <div class="window">
+                <h2>About this page</h2>
+                <div class="content">
+ 					
+                </div>
+                </div>
+            </div>
+        </div>
+    </body>
