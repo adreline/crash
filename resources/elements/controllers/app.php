3b34ea4a07fda6b43cd0a7d62dc34046033b8618
Ubuntu <maddie.madlad@gmail.com>
2022-04-19 18:00:43 +0200

    created logic for the apps default controller

--- resources/elements/controllers/app.php
+++ resources/elements/controllers/app.php
@@ -1,3 +1,24 @@
 <?php
-	function 
+require_once Crash::$element['page'];
+/*
+* This controller processes default pages. usually static pages.
+*/
+	function process($req, $template='default'){
+		//check if requested page is present in static pages 
+		if (isset(Crash::$static_page[$req])) {
+			//reneder the page
+			include Crash::$static_page[$req];
+		}else{
+			//check if requested page is present in the database
+			$page_id = Page::fetchPageByName($req);
+			if ($page_id!=-1) {
+				//render the page
+				$page = Page::getPage($page_id);
+				include Crash::$template[$template];
+			}else{
+				//page cant be found, return 404
+				header('HTTP/1.1 404 Not Found');
+			}
+		}
+	}
 ?>
