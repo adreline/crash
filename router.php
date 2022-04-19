54265d2987b7e93ba58ad97abcc3889c3d829df2
Ubuntu <maddie.madlad@gmail.com>
2022-04-19 17:59:06 +0200

    router now uses new route/controller logic

--- router.php
+++ router.php
@@ -1,18 +1,32 @@
 <?php 
-require "resources/elements/app.php";
+require "resources/elements/crash.php";
 require Crash::$element['page'];
-
-if(isset($uris[1])&&strlen($uris[1])!=0){
-    $route = $uris[1];
-    $page_id = Page::fetchPageByName($route);
-    if($page_id!=-1){
-        $page = Page::getPage($page_id);
-        include Crash::$template['default'];
+/**
+* This file is first point of reference for all requests.
+* From here, we decide how to process incoming requests.
+* Generally, it should not be edited. 
+*/
+$req = Helper::assertRoute();
+if (isset($req)) {
+    //decide which controller to use 
+    if (sizeof($req)>2) {
+        //if more then 2, it means we need to pass this request to specified controller
+        //look for a controller
+        if (isset(Crash::$controller[$req[1]])) {
+            //controller exists, pass request to it
+            require Crash::$controller[$req[1]];
+            process($req[2]);
+        }else{
+            //controller was not found return 404
+            header('HTTP/1.1 404 Not Found');
+        }
     }else{
-        header('HTTP/1.1 404 Not Found');
+        //if the request only have 2 fields, that means it will use default controller
+        require Crash::$controller['app'];
+        process($req[1]);
     }
 }else{
-    include Crash::$static_page['home'];
+     header('HTTP/1.1 404 Not Found');
 }
 
 ?>
