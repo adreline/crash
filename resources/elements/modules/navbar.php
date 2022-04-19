83e0999de2945bd22e9e076b464e0f661e8d7df8
Ubuntu <maddie.madlad@gmail.com>
2022-04-19 19:27:25 +0200

    added new page to navbar

--- resources/elements/modules/navbar.php
+++ resources/elements/modules/navbar.php
@@ -9,12 +9,13 @@
                     <p>maddieuwu@search<mark class="info">:</mark>~<mark class="info">$</mark><input type="text"></p>
                 </div>
                 <ul>
-                    <li><a href="http://niecko.4suns.pl/crash/">Home</a></li>
+                    <li><a href="http://niecko.4suns.pl/crash/">Home</a></li>             
                     <?php
                         foreach (Page::getPage() as $page){
-                            echo "<li><a class=\"is-active\" href=\"$page->name\">$page->friendly_name</a></li>";
+                            echo "<li><a href=\"$page->name\">$page->friendly_name</a></li>";
                         }
                     ?>
+                    <li><a href="http://niecko.4suns.pl/crash/about">What is this?</a></li>
                 </ul>
             </div>
 </nav>
