7c47f3189fe1a7b465c57f9bb13f89da632dd5f8
Ubuntu <maddie.madlad@gmail.com>
2022-05-17 14:02:26 +0200

    QoL changes

--- resources/views/templates/modules/user_banner.php
+++ resources/views/templates/modules/user_banner.php
@@ -9,18 +9,19 @@
     <div class="column">
       <p>Followers: n/a</p>
       <p>Kudos: <?php echo Elements\Kudo::countReceivedUserKudosById($user->id); ?></p> 
-      <div class="inline">
+      <div class="grid">
         <a href="/crash/users/profile"><mark class="success">[profile]</mark></a>
-        <form action="/crash/users/logout" method="post">
-          <button name="submit" type="submit"><mark class="danger">[logout]</mark></button>
-        </form>
-      </div>
-       <?php
+        <a href="/crash/users/scriptorium"><mark class="success">[go to author board]</mark></a>
+        <?php
           //attach admin routes but only when the user have necessery privelage
         if($user->administrator){
           echo "<a href=\"/crash/admin\"><mark class=\"info\">[administrative]</mark></a>";
         }
        ?>
+       <form action="/crash/users/logout" method="post">
+          <button name="submit" type="submit"><mark class="danger">[logout]</mark></button>
+        </form>
+      </div>
     </div>
     </div>
     <p>Recent activity</p>
