cc3bf52af7bf6ded2680ab49e6d1680d067757ce
Ubuntu <maddie.madlad@gmail.com>
2022-05-18 10:31:29 +0200

    improved search by using similar_text built in function

--- resources/elements/controllers/search.php
+++ resources/elements/controllers/search.php
@@ -12,6 +12,24 @@ class Controller{
     public static function search($query){
         $query = htmlspecialchars(strip_tags(addslashes(strtolower($query))));
         $publications = Publication::getPublication();
+        //we sort publications according to this comparison function passed to usort
+        $sorter = function ($q){
+            return function($a, $b) use ($q){
+                 similar_text($a->title, $q, $a_sim);
+                 similar_text($b->title, $q, $b_sim);
+                 if($a_sim == $b_sim) return 0;
+                 return ($a_sim > $b_sim) ? -1 : 1;
+             };
+         };
+         usort($publications, $sorter($query));
+        include Crash::$static_page['search'];
+
+    }
+
+
+    public static function search_depreciated($query){
+        $query = htmlspecialchars(strip_tags(addslashes(strtolower($query))));
+        $publications = Publication::getPublication();
 
         //we sort publications according to this comparison function passed to usort
         $sorter = function ($q){
